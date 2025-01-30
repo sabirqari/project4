d3ff272c7014b8727e053e721140f694ee1d0dbe
nasrinna <nasrin.ahmad@noorteck.com>
2025-01-29 21:04:55 -0500

    this a new code

--- src/Demo1/DemoTest4.java
+++ src/Demo1/DemoTest4.java
@@ -4,5 +4,12 @@ public class DemoTest4 {
 
 	public static void main(String[] args) {
 		System.out.println("Java");
+		
+		
+		int a = 10 ;
+        int b = 20;
+        int c  = 30;
+        System.out.println( a + b + c);
+        
 	}
 }
